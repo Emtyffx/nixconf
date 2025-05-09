738f6eb552ccc270291718abf6e3c4cf97211540
Emtyffx <p.verbytsky@gmail.com>
2025-05-09 08:17:02 +0300

    Update inputs

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -223,9 +223,16 @@
   ];
 
   # enable postgres(for development)
-  services.postgresql.enable = true;
-  services.postgresql.package = pkgs.postgresql_17;
-
+  services.postgresql = {
+    enable = true;
+    enableTCPIP = true;
+    package = pkgs.postgresql;
+    authentication = pkgs.lib.mkOverride 10 ''
+      local all    all   trust
+      host  all    all   127.0.0.1/32  trust
+      host  all    all   ::1/128       trust
+    '';
+  };
   # system variables
   environment.sessionVariables = {
     SUDO_EDITOR = "nvim";
